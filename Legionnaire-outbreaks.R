14dccab8ca76c06cca08c0f24652808027006671
Holly Kessler <hakess@rams.colostate.edu>
2016-07-18 10:53:36 -0600

    update

--- Legionnaire-outbreaks.R
+++ Legionnaire-outbreaks.R
@@ -1,7 +1,6 @@
 #install_github("ropenscilabs/rnoaa")
 library(devtools)
 library(rnoaa)
-library(ggmap)
 library(countyweather)
 library(dplyr)
 library(plyr)
@@ -97,22 +96,22 @@ outbreak_loc_true$stations <- NULL
 # Only need to run once to save the data 
 
 #for(i in which(has_stations))
-#{
-   meteo_df <- meteo_pull_monitors(monitors = stations[[i]]$id,
-                                keep_flags = FALSE,
-                                date_min = outbreak_loc$date_min[i],
-                                date_max = outbreak_loc$date_max[i],
-                                var = c("prcp","snow","snwd","tmax","tmin","tavg"))
-  coverage_df <- meteo_coverage(meteo_df, verbose = FALSE)
-  filtered <- filter_coverage(coverage_df, 0.90)
-  good_monitors <- unique(filtered$id)
-  filtered_data <- filter(meteo_df, id %in% good_monitors)
-  averaged <- ave_weather(filtered_data)
-
-  file_name <- paste0("weather_files/", outbreak_loc$file_id[i], ".rds")
-  saveRDS(averaged, file_name)
-  #readRDS(file_name)
-}
+# {
+#    meteo_df <- meteo_pull_monitors(monitors = stations[[i]]$id,
+#                                 keep_flags = FALSE,
+#                                 date_min = outbreak_loc$date_min[i],
+#                                 date_max = outbreak_loc$date_max[i],
+#                                 var = c("prcp","snow","snwd","tmax","tmin","tavg"))
+#   coverage_df <- meteo_coverage(meteo_df, verbose = FALSE)
+#   filtered <- filter_coverage(coverage_df, 0.90)
+#   good_monitors <- unique(filtered$id)
+#   filtered_data <- filter(meteo_df, id %in% good_monitors)
+#   averaged <- ave_weather(filtered_data)
+# 
+#   file_name <- paste0("weather_files/", outbreak_loc$file_id[i], ".rds")
+#   saveRDS(averaged, file_name)
+#   #readRDS(file_name)
+#  }
 
 
 ###DATA ANALYZE###
@@ -242,7 +241,8 @@ for(i in 1:length(list.files("weather_files")))
                aes(xintercept = value, color = day_in_seq), 
                alpha = 0.25) + 
     xlim(c(-10,30)) + ylim(c(0, 300)) +
-    ggtitle(city_name)
+    ggtitle(city_name) +
+    scale_color_gradientn(colors=rainbow(4))
   print(c)
   
   #percentiles
@@ -260,5 +260,6 @@ for(i in 1:length(list.files("weather_files")))
 
 # PLOT 7: SNWD
 
-
-#?articles
+# log scale 
+# riem, first find station number
+# riem_networks
